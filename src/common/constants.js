0e6964a41bfc602d8a93e0cd85b8bd59ba3bc373
Xinyu Wang <wang.xinyu@husky.neu.edu>
2020-03-14 17:36:54 -0400

    change service url

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,7 +1,7 @@
 export const NUID = '001051600';
 export const SERVICE_URL = `https://wbdv-generic-server.herokuapp.com/api/${NUID}`;
-export const JPA_SERVICE_URL = "http://localhost:8080";
-// export const WIDGET_SERVICE_URL = "https://evening-bastion-56155.herokuapp.com";
+// export const JPA_SERVICE_URL = "http://localhost:8080";
+export const WIDGET_SERVICE_URL = "https://morning-tor-14549.herokuapp.com";
 export const INPUT_PICKER_VALUE = [
     {"value": "Heading", "label": "Heading"},
     {"value": "Paragraph", "label": "Paragraph"},
